c4ae76c9ba0f6028a4fabffbc0a2e0e0886f9a4c
berkbavas <berkbavas@gmail.com>
2024-10-13 23:36:31 +0300

    Unused files are removed.

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,30 +1,10 @@
 #include "Core/Controller.h"
 
 #include <QApplication>
-#include <QFile>
-#include <QFontDatabase>
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
-
-    int id = QFontDatabase::addApplicationFont(":/Resources/Fonts/Segoe/Segoe-UI-Variable-Static-Text.ttf");
-    if (id == -1)
-    {
-        QFont font = qApp->font();
-        font.setPixelSize(12);
-        font.setBold(false);
-        qApp->setFont(font);
-        qDebug() << "Font cannot be loaded. Using default font:" << qApp->font();
-    }
-    else
-    {
-        QString family = QFontDatabase::applicationFontFamilies(id).at(0);
-        QFont font(family);
-        font.setPixelSize(12);
-        qApp->setFont(font);
-        qDebug() << "Font is loaded. Using" << qApp->font();
-    }
 
     FovCalculator::Controller controller;
     controller.Run();
